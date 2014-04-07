2db9a3e81227dedf6369a71e9d794dbc559573e2
Philip Jagielski <philipjagielski@gmail.com>
2014-04-06 23:49:02 -0700

    setup.bat now installs all relevant settings in Windows

--- setup.bat
+++ setup.bat
@@ -1,10 +1,26 @@
 @echo off
 
-REM TODO: install vim stuff
-REM TODO: install the autohotkey script in the startup folder
+:: TODO: install the autohotkey script in the startup folder
 
-REM Vim and GVim first looks in %HOME% and if it isn't defined then %HOMEDRIVE + %HOMEPATH%
-REM the file is named _vimrc
+:: install vimrc (just copy over old one :-/)
+call cp .vimrc %home%\_vimrc
 
-REM If this isn't working, open vim and type :version
-REM This will tell you where it is loading everything from.
+:: install vundle
+if not exist "%home%\.vim\bundle\vundle\" (
+   mkdir -p %home%\.vim\bundle\vundle\
+   git clone https://github.com/gmarik/vundle.git %home%\.vim\bundle\vundle\
+   vim +BundleInstall! +qall
+) else (
+   echo "Run vim +BundleInstall! +qall to upgrade vim plugins"
+)
+
+:: Configure git
+git config --global color.ui true
+git config --global user.name "Philip Jagielski"
+git config --global user.email "philipjagielski@gmail.com"
+git config --global core.editor vim
+git config --global merge.tool vimdiff
+
+:: install mercurial stuff
+call cp mercurial\mercurial.ini %home%\mercurial.ini
+call cp mercurial\hgignore.ini %home%\hgignore.ini
